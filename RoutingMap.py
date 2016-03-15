6c393917f88ec3b16620b7a1a4309b7a3297aeb4
Will <teck.geek@hotmail.com>
2016-03-14 19:44:26 -0600

    Making it understandable
    adding lots of stuff for making it more readable

--- RoutingMap.py
+++ RoutingMap.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python2.7
 #This Python file uses the following encoding: utf-8
 __author__ = """\n""".join(['Will CSCI 305 Programming Lab 2 — Reconstructing Montana’s Road Network'])
-from collections import defaultdict
-from collections import deque
+from collections import defaultdict, deque
 import fileinput, string, optparse, os, sys, getopt, re
-try:
+try: #checking to make sure networkx is installed
     import networkx as nx
 except ImportError, e:
-    try:
+    try: #if not installed install it
         import pip
         pip.main(['install', 'networkx'])
         import networkx as nx
     except ImportError, e:
         print "Cant install networkx please resolve this first"
-roudmap=nx.MultiGraph()
-cities=[]
-bad_chars = """[!@#$ -,)(*&^%<>?/\r\n"|{}=+.]:; """
+roudmap=nx.MultiGraph() #setting up graph for MultiGraph
+cities=[] #list of cities in graph
+bad_chars = """[!@#$ -,)(*&^%<>?/\r\n"|{}=+.]:; """ #list of char to remove from city names
 
-class Town: #building definition
+class Town: #building city object for Town
     def __init__(self,name):
         self.name = name
-def mystrip(strg):
+def mystrip(strg): #strip &bad_char from string
     """
     Strips bad characters from a string.
     """
     return strg.translate(string.maketrans("", "", ), bad_chars)
 
-def breadth_first_search(g, source):
+def breadth_first_search(g, source): #breadth_first_search algorithm for finding path to nodes
      queue = deque([(None, source)])
      enqueued = set([source])
      while queue:
@@ -36,7 +35,7 @@ def breadth_first_search(g, source):
          enqueued |= new
          queue.extend([(n, child) for child in new])
 
-def striplist(l):
+def striplist(l): #remove white space from list
     return([x.strip() for x in l])
 
 def parseFile(cityFile):  #building town object dictionary
@@ -64,10 +63,10 @@ def parseFile(cityFile):  #building town object dictionary
                 cities.append(info[1])
             add2graph(info[0], info[1], info[2])
     print "Done importing cities", cities
-def map():
+def map(): #print map of graph
     for n1, n2, attr in roudmap.edges(data=True): # unpacking
         print n1, n2, attr['weight']
-def directConnection():
+def directConnection(): #find if town a is directly connected to town b
     places = ""
     while places != quit:
         places = raw_input("Please enter the cities to query\n or quit to exit: ").strip().lower()
@@ -86,7 +85,7 @@ def directConnection():
                 print "No"
     return
 
-def add2graph(city1, city2, miles):
+def add2graph(city1, city2, miles): #add lots of stuff to graph, building graph
     if city1 not in roudmap:
         #print "adding", city1
         roudmap.add_node(city1)
@@ -96,28 +95,29 @@ def add2graph(city1, city2, miles):
     if city1 in roudmap:
         #print "Adding distance", miles
         roudmap.add_edge(city1, city2, weight=miles)
-def quarryAgacent():
+def quarryAgacent(): # see how many towns are agecent to the chosen town
     city = raw_input("Please enter the city to query\n").strip().lower()
     if city in cities:
         print city, " is agacent to ", len(roudmap.edges(city)), " Cities"
         return
     print "No city found please try agian"
-def checkKhop():
+def checkKhop(): #c if possible to reace destination from starting point within d times
     places = ""
     while places != quit:
         places = raw_input("Please enter the cities to query\n or quit to exit: ").strip().lower()
         places = places.split()
         places = striplist(places)
-        places[0]=mystrip(places[0])
-        places[1]=mystrip(places[1])
-        d = places[2]
         if len(places) != 3:
             print "Wrong amount of entries please try agian"
             return
         else:
-            if nx.haspath(roudmap,places[0],places[1]):
-                print "Yes, 1", roudmap.edge[places[0]][places[1]]
-            else:
+            places[0]=mystrip(places[0])
+            places[1]=mystrip(places[1])
+            d = places[2]
+            try:
+                roudmap.edge[places[0]][places[1]]
+                print roudmap.edge[places[0]][places[1]]
+            except:
                 print breadth_first_search(places[1], places[0])
     return
 
@@ -164,5 +164,5 @@ def main(argv): #main argv for input
     parseFile(cityFile)
     userInput()
 
-if __name__ == "__main__":
+if __name__ == "__main__": #send python to main loop to start
    main(sys.argv[1:])
